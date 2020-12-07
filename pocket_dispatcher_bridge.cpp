88911b93aef741ff134a150583e6c32407978ca0
Ben <benrcarver@gmail.com>
2020-12-07 18:19:12 -0500

    Replaced string with char* and then convert to std::string when calling pocket code

--- pocket_dispatcher_bridge.cpp
+++ pocket_dispatcher_bridge.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "pocket_dispatcher_bridge.h"
 #include "pocket_dispatcher.h"
@@ -13,59 +14,59 @@ void* pocket_NewPocketDispatcher() {
     return dispatcher;
 }
 
-int pocket_Initialize(void* pocketDispatcher, string address, int pocket_port) {
+int pocket_Initialize(void* pocketDispatcher, char* address, int pocket_port) {
     std::cout << "[C++ Bridge] pocket_Initialize" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->Initialize(address, pocket_port);
+    return AsPocketDispatcher(pocketDispatcher)->Initialize(str(address), pocket_port);
 }
 
-int pocket_MakeDir(void* pocketDispatcher, string name) {
+int pocket_MakeDir(void* pocketDispatcher, char* name) {
     std::cout << "[C++ Bridge] pocket_MakeDir" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->MakeDir(name);
+    return AsPocketDispatcher(pocketDispatcher)->MakeDir(str(name));
 }
 
-int pocket_Lookup(void* pocketDispatcher, string name) {
+int pocket_Lookup(void* pocketDispatcher, char* name) {
     std::cout << "[C++ Bridge] pocket_Lookup" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->Lookup(name);
+    return AsPocketDispatcher(pocketDispatcher)->Lookup(str(name));
 }
 
-int pocket_Enumerate(void* pocketDispatcher, string name) {
+int pocket_Enumerate(void* pocketDispatcher, char* name) {
     std::cout << "[C++ Bridge] pocket_Enumerate" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->Enumerate(name);
+    return AsPocketDispatcher(pocketDispatcher)->Enumerate(str(name));
 }
 
-int pocket_PutFile(void* pocketDispatcher, string local_file, string dst_file, bool enumerable) {
+int pocket_PutFile(void* pocketDispatcher, char* local_file, char* dst_file, bool enumerable) {
     std::cout << "[C++ Bridge] pocket_PutFile" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->PutFile(local_file, dst_file, enumerable);
+    return AsPocketDispatcher(pocketDispatcher)->PutFile(str(local_file), str(dst_file), enumerable);
 }
 
-int pocket_GetFile(void* pocketDispatcher, string src_file, string local_file) {
+int pocket_GetFile(void* pocketDispatcher, char* src_file, char* local_file) {
     std::cout << "[C++ Bridge] pocket_GetFile" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->GetFile(src_file, local_file);
+    return AsPocketDispatcher(pocketDispatcher)->GetFile(str(src_file), str(local_file));
 }
 
-int pocket_PutBuffer(void* pocketDispatcher, const char buf[], int pocket_len, string dst_file, bool enumerable) {
+int pocket_PutBuffer(void* pocketDispatcher, const char buf[], int pocket_len, char* dst_file, bool enumerable) {
     std::cout << "[C++ Bridge] pocket_PutBuffer" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->PutBuffer(buf, pocket_len, dst_file, enumerable);
+    return AsPocketDispatcher(pocketDispatcher)->PutBuffer(buf, pocket_len, str(dst_file), enumerable);
 }
 
-int pocket_GetBuffer(void* pocketDispatcher, char buf[], int pocket_len, string src_file) {
+int pocket_GetBuffer(void* pocketDispatcher, char buf[], int pocket_len, char* src_file) {
     std::cout << "[C++ Bridge] pocket_GetBuffer" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->GetBuffer(buf, pocket_len, src_file);
+    return AsPocketDispatcher(pocketDispatcher)->GetBuffer(buf, pocket_len, str(src_file));
 }
 
-int pocket_DeleteFile(void* pocketDispatcher, string file) {
+int pocket_DeleteFile(void* pocketDispatcher, char* file) {
     std::cout << "[C++ Bridge] pocket_DeleteFile" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->DeleteFile(file);
+    return AsPocketDispatcher(pocketDispatcher)->DeleteFile(str(file));
 }
 
-int pocket_DeleteDir(void* pocketDispatcher, string directory) {
+int pocket_DeleteDir(void* pocketDispatcher, char* directory) {
     std::cout << "[C++ Bridge] pocket_DeleteDir" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->DeleteDir(directory);
+    return AsPocketDispatcher(pocketDispatcher)->DeleteDir(str(directory));
 }
 
-int pocket_CountFiles(void* pocketDispatcher, string directory) {
+int pocket_CountFiles(void* pocketDispatcher, char* directory) {
     std::cout << "[C++ Bridge] pocket_CountFiles" << std::endl;
-    return AsPocketDispatcher(pocketDispatcher)->CountFiles(directory);
+    return AsPocketDispatcher(pocketDispatcher)->CountFiles(str(directory));
 }
 
 void pocket_DestroyPocketDispatcher(void* pocketDispatcher) {
